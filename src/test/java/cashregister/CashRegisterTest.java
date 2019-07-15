7ab27d89e7d34dfe0962638e7bde68f5737cb200
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-15 17:51:56 +0800

    Update CashRegisterTest.java

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,32 +1,41 @@
-package cashregister;
-
+package org.katas.refactoring;
 
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
+import java.util.ArrayList;
 
-public class CashRegisterTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
 
+public class OrderReceiptTest {
     @Test
-    public void should_print_the_real_purchase_when_call_process() {
-        //given
-        //when
-        //then
+    public void shouldPrintCustomerInformationOnOrder() {
+        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
+        OrderReceipt receipt = new OrderReceipt(order);
+
+        String output = receipt.printReceipt();
+
+        assertThat(output).contains("Mr X", "Chicago, 60601");
     }
 
     @Test
-    public void should_print_the_stub_purchase_when_call_process() {
-        //given
-        //when
-        //then
-    }
+    public void shouldPrintLineItemAndSalesTaxInformation() {
+        ArrayList<LineItem> lineItems = new ArrayList<LineItem>() {{
+            add(new LineItem("milk", 10.0, 2));
+            add(new LineItem("biscuits", 5.0, 5));
+            add(new LineItem("chocolate", 20.0, 1));
+        }};
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
 
-    @Test
-    public void should_verify_with_process_call_with_mockito() {
-        //given
-        //when
-        //then
+        String output = receipt.printReceipt();
+
+        assertThat(output).contains(
+                "milk\t10.0\t2\t20.0\n",
+                "biscuits\t5.0\t5\t25.0\n",
+                "chocolate\t20.0\t1\t20.0\n",
+                "Sales Tax\t6.5",
+                "Total Amount\t71.5"
+        );
     }
 
 }
